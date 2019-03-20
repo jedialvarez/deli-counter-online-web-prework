1f58e9322c95649ba35743eed9a3bdb7ac2a2e38
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-20 17:45:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
+def line(another_deli)
+    if another_deli.length == 0
+          puts  "The line is currently empty."
+        elsif another_deli.length >= 1
+          another_deli.each {|name| print name}
+        else another_deli.take
+    end
+end
+
+def take_a_number(katz_deli, name)
+katz_deli.length == 0
+katz_deli.push(1)
+end
+
+def now_serving
+end
+
+
+
+
+
 # Write your code here.
